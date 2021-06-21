a2bfa9816cfb41f81aaacc02e7ee03cfc8efdb7c
Derek Williams <dwilli77@gmail.com>
2021-06-21 17:04:44 -0400

    added support file for valid google books ids for testing purposes

---
+++ spec/support/valid_google_ids.rb
@@ -0,0 +1,53 @@
+# frozen_string_literal: true
+
+module ValidGoogleIds
+  def self.valid_google_ids
+    [
+      'B1hSG45JCX4C',
+      'rPufswEACAAJ',
+      '4kbYExSCa6oC',
+      '1N1rTDJP55UC',
+      'ppO3QgAACAAJ',
+      '-hmtDwAAQBAJ',
+      'ypLqDwAAQBAJ',
+      'lH_TDwAAQBAJ',
+      '8pHqDwAAQBAJ',
+      '9QYJe3eEbu0C',
+      'GNnxzQEACAAJ',
+      'aWZzLPhY4o0C',
+      'AVVoPwAACAAJ',
+      'yl4dILkcqm4C',
+      'lqHNugAACAAJ',
+      'AN8TDQAAQBAJ',
+      'CQAVMQAACAAJ',
+      'nh0eAQAAIAAJ',
+      'oaIRrgEACAAJ',
+      'GEWXQbASXZUC',
+      'ePuCDQAAQBAJ',
+      'ot_AYAVwokkC',
+      'aB4B13xGEv4C',
+      'CHx7Cb9a32cC',
+      'F2jyrQEACAAJ',
+      'BaWayIR0CvkC',
+      '9nmF5-7qH8sC',
+      'JJ7SHIRDfOcC',
+      'i6mZ0HBDPzsC',
+      '7XPgywEACAAJ',
+      'ptiYBAAAQBAJ',
+      '4RChxt67lvwC',
+      'p1v6DwAAQBAJ',
+      'aAMrswEACAAJ',
+      'PXa2bby0oQ0C',
+      'tXGRBgwxAHIC',
+      'YhaUjgEACAAJ',
+      '9C1hDwAAQBAJ',
+      '-QcZswEACAAJ',
+      'cxNVDgAAQBAJ',
+      '1VdJlwEACAAJ',
+      'CehFAQAAQBAJ',
+      'A_-zBAAAQBAJ',
+      'Hn41AgAAQBAJ',
+      'JZpeDwAAQBAJ'
+    ]
+  end
+end
