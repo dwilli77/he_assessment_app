b996525a149455fcdcaff6d08fcb203671178ccb
Derek Williams <dwilli77@gmail.com>
2021-06-21 16:54:27 -0400

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users, only: [:create] do
+    post :grab_token, on: :collection
+  end
+
+  resources :books, only: %i[index create update destroy]
+
+  get 'query', to: 'searches#search'
 end
