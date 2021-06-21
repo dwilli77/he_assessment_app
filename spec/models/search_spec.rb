095d1956b44cf494cf718ab7402d5f5c745690ea
Derek Williams <dwilli77@gmail.com>
2021-06-21 17:29:36 -0400

    added tests for searches

--- spec/models/search_spec.rb
+++ spec/models/search_spec.rb
@@ -1,5 +1,13 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe Search, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'for duplicate searches' do
+    it 'does not create a duplicate on Search table' do
+      search = FactoryBot.create(:search, term: 'Pumpkin')
+      search_2 = FactoryBot.build(:search, term: 'Pumpkin')
+      expect { search_2.save! }.to raise_error(ActiveRecord::RecordInvalid)
+    end
+  end
 end
--- spec/requests/searches_spec.rb
+++ spec/requests/searches_spec.rb
@@ -1,7 +1,41 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
-RSpec.describe "Searches", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+RSpec.describe 'Searches', type: :request do
+  def valid_headers
+    {
+      'Authorization' => "Bearer #{user.encode_token}"
+    }
+  end
+
+  describe 'GET /search' do
+    let(:user) { FactoryBot.create(:user) }
+
+    context 'when querying a multiple times' do
+      it 'only creates one search on duplicate' do
+        get query_path, headers: valid_headers, params: { search_term: 'Dune' }
+        get query_path, headers: valid_headers, params: { search_term: 'Dune' }
+        expect(Search.all.count).to be(1)
+      end
+      it 'creates multiple searches if no duplication' do
+        get query_path, headers: valid_headers, params: { search_term: 'Dune' }
+        get query_path, headers: valid_headers, params: { search_term: 'Children of Dune' }
+        expect(Search.all.count).to be(2)
+      end
+      it "adds to user's search history whether it's a duplicate or not" do
+        get query_path, headers: valid_headers, params: { search_term: 'Dune' }
+        get query_path, headers: valid_headers, params: { search_term: 'Dune' }
+        expect(user.searches.count).to be(2)
+      end
+    end
+
+    context 'when the query is empty' do
+      it 'creates an empty search' do
+        get query_path, headers: valid_headers
+        expect(Search.last.response_status).to be(400)
+        expect(Search.last.response_body).to be_nil
+      end
+    end
   end
 end
